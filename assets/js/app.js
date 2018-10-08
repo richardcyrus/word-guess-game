e4bd85905c39f643138cb8f8419651f8e0155f26
Richard Cyrus <richard.cyrus@rcyrus.com>
2018-10-07 21:54:00 -0400

    Most of the game's core JS is complete.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 /**
  * Word Guess Game.
  *
- * UNCC Full Stack Boot Camp.
+ * UNC Charlotte Full Stack Boot Camp.
  * (c) 2018 Richard Cyrus <richard.cyrus@rcyrus.com>
  *
  * 1.   The computer chooses a word, and displays an underscore for each
@@ -19,147 +19,212 @@
  *
  */
 
-(function (window, undefined) {
+
+(function (window) {
     'use strict';
 
-    let WordGuess = {
+    // Track the number of winning games for the player.
+    let numberOfWins = 0;
 
-        // Research this more. It doesn't work without this!
+    let WordGuess = {
+        words: [],
+        // correctGuesses: 0,
+        wrongLetters: [],
+        correctLetters: [],
+        // maximumAttempts: 0,
+        remainingAttempts: 0,
+        elAttemptsLeft: document.querySelector('#attempts-remaining'),
+        elGamesWon: document.querySelector('#games-won'),
+        elCurrentWord: document.querySelector('#current-word'),
+        elNotices: document.querySelector('#notices'),
+        elWrongLetters: document.querySelector('#wrong-letters'),
+        elWrongLetterTitle: document.querySelector('.used-letters > p'),
+        elHint: document.querySelector('.hint'),
+        currentWord: {},
+
+        /**
+         * Populate the word list, choose the first word of play, and
+         * start the game.
+         *
+         * @param words
+         */
         init: function(words) {
-            this.words = words,
-            this.correctGuesses = [],
-            this.wrongGuesses = [],
-            this.correctLetters = 0,
-            this.maximumAttempts = 0,
-            this.elAttemptsLeft = document.querySelector('#attempts-remaining'),
-            this.elGamesWon = document.querySelector('#games-won'),
-            this.elCurrentWord = document.querySelector('#current-word'),
-            this.elNotices = document.querySelector('#notices'),
-            this.elWrongLetters = document.querySelector('#wrong-letters'),
-            this.elWrongLetterTitle = document.querySelector('.used-letters > p'),
-            this.elHint = document.querySelector('.hint'),
-            this.currentWord = this.chooseWord(),
+            this.words = words;
+            this.currentWord = this.chooseWord();
             this.setup();
+
+            console.log(this.currentWord);
         },
 
         setup: function() {
             this.startPlay();
         },
 
-        resetGame: function() {
-            this.init(this.words);
-            this.showBadGuesses();
-        },
-
-        chooseWord: function() {
-            let _choice = this.words[Math.floor(Math.random() * this.words.length)];
-            this.maximumAttempts = _choice.length + 8;
-
-            let _letters = [];
-            for (let i = 0; i < _choice.length; i++ ){
-                _letters.push({
-                    letter: _choice[i],
-                    pos: i
-                });
-            }
-
-            return {
-                letters: _letters,
-                word: _choice.toLowerCase(),
-                totalLetters: _choice.length
-            };
-        },
-
-        startPlay: function() {
-            let el = '<ul class="word">';
-
-            for (let i = 0; i < this.currentWord.totalLetters; i++) {
-                el += '<li data-pos="' + i + '" class="letter">*</li>';
-            }
-
-            el += '</ul>';
-
-            this.elCurrentWord.innerHTML = el;
-            this.elAttemptsLeft.innerHTML = this.maximumAttempts;
-        },
-
-        showBadGuesses: function() {
-            if (this.wrongGuesses) {
-
-                let el = '<ul class="wrong-letters">';
-
-                for (let item of this.wrongGuesses) {
-                    el += '<li>' + item + '</li>';
-                }
-
-                el += '</ul>';
-                this.elWrongLetterTitle.classList.remove('hidden');
-                this.elWrongLetters.innerHTML = el;
-            } else {
-                this.elWrongLetterTitle.classList.add('hidden');
-                this.elWrongLetters.innerHTML = "";
-            }
-        },
-
-        playerGuess: function(event) {
+        /**
+         * Key press entry point into the game. When the player presses a key
+         * it's processed here.
+         *
+         * @param event
+         */
+        playerGuess: function (event) {
             let guess = event.key;
-            console.log(guess);
 
-            if (guess.match(/[a-zA-Z]/) && guess.length === 1) {
-                if (this.wrongGuesses.indexOf(guess) > -1 || this.correctGuesses.indexOf(guess) > -1) {
+            if (this.remainingAttempts === 0) {
+                // Game Over.
+                this.resetGame();
+            } else if (guess.match(/[a-zA-Z]/) && guess.length === 1) {
+
+                if (this.wrongLetters.includes(guess) || this.correctLetters.includes(guess)) {
+                    // TODO: Use a flash message instead of an alert.
                     alert('You have tried that letter before. Please try again!');
+
                     // TODO: Play sound (failed/bad)
                 }
                 else {
-                    let solvedLetters = this.validateGuess(guess);
+                    let validCharacters = this.validateGuess(guess);
 
-                    if (solvedLetters.length > 0) {
-                        this.updateBoard(solvedLetters);
-                        // TODO: Play sound (success!)
-                    } else {
-                        this.wrongGuesses.push(guess);
-                        if (this.wrongGuesses.length === this.maximumAttempts) {
-                            // Game Over
-                            // this.playerLoses()
-                        } else {
-                            this.showBadGuesses();
-                            // TODO: Play Sound (failed/bad)
+                    if (validCharacters.length > 0) {
+
+                        this.updateBoard(validCharacters);
+
+                        if (this.correctLetters.length === this.currentWord.totalCharacters) {
+
+                            this.elAttemptsLeft.innerHTML = '0';
+                            numberOfWins += 1;
+                            // TODO: Play sound (success!)
+                            this.playerWins();
+                            this.resetGame();
                         }
+                    } else {
+                        // Deduct an attempt for every invalid letter press.
+                        this.remainingAttempts = this.remainingAttempts - 1;
+                        this.elAttemptsLeft.innerHTML = this.remainingAttempts;
+
+                        this.wrongLetters.push(guess);
+                        this.elWrongLetterTitle.classList.remove('hidden');
+
+                        this.showBadGuesses();
+                        // TODO: Play Sound (failed/bad)
                     }
                 }
             }
         },
 
+        /**
+         * Determine if the character entered by the player is in the
+         * current word.
+         *
+         * @param guess
+         * @returns {Array}
+         */
         validateGuess: function (guess) {
-            let valid = [];
+            // Capture all instances of the guessed letter in the word.
+            let valid = this.currentWord.characters.filter(function(letter){
+                return guess.toLowerCase() === letter.character
+            });
 
-            for (let l of this.currentWord.letters) {
-                if (guess.toLowerCase() === l.letter.toLowerCase()) {
-                    valid.push(l);
-                    this.correctGuesses.push(l.letter);
-                }
-            }
+            // Update the correctLetters list.
+            valid.forEach(function (letterObject) {
+                this.correctLetters.push(letterObject.character);
+            }, this);
 
             return valid;
         },
 
-        updateBoard: function (choices) {
+        /**
+         * Update the game display with the correctly guessed letters.
+         *
+         * @param letters
+         */
+        updateBoard: function (letters) {
+            letters.forEach(function (letter) {
+                let listItem = this.elCurrentWord.querySelector(
+                    'li[data-pos="' + letter.pos + '"]'
+                );
 
-            this.correctLetters = this.correctLetters += choices.length;
-
-            for (let letter of choices) {
-                let listItem = this.elCurrentWord.querySelector('li[data-pos="' + letter.pos + '"]');
-                listItem.innerHTML = letter.letter;
+                listItem.innerHTML = letter.character;
                 listItem.classList.add('correct');
+            }, this);
+        },
 
-                if (this.correctLetters === this.currentWord.totalLetters) {
-                    this.playerWins();
-                }
+        /**
+         * Selects a word for play.
+         *
+         * @param increaseMaxAttempts
+         * @returns {{characters: Array, word: string, totalCharacters: *}}
+         */
+        chooseWord: function(increaseMaxAttempts = 2) {
+            let choice = this.words[Math.floor(Math.random() * this.words.length)];
+
+            this.remainingAttempts = choice.length + increaseMaxAttempts;
+
+            let letters = [];
+            for (let i = 0; i < choice.length; i++ ){
+                letters.push({
+                    character: choice[i],
+                    pos: i
+                });
             }
+
+            return {
+                characters: letters,
+                word: choice.toLowerCase(),
+                totalCharacters: choice.length
+            };
+        },
+
+        /**
+         * Display the game board. There's one `_` for each character of
+         * the currently active word.
+         *
+         * This also displays the initial attempts remaining.
+         */
+        startPlay: function() {
+            let el = '<ul class="word">';
+
+            for (let i = 0; i < this.currentWord.totalCharacters; i++) {
+                el += `<li data-pos="${i}" class="letter">*</li>`;
+            }
+
+            el += '</ul>';
+
+            this.elCurrentWord.innerHTML = el;
+
+            // Set at start.
+            this.elAttemptsLeft.innerHTML = this.remainingAttempts;
+        },
+
+        /**
+         * When the wrong choice is made in the game, update the
+         * letters used portion of the screen with the letter that
+         * was chosen.
+         */
+        showBadGuesses: function() {
+            let el = '<ul class="used-letters">';
+
+            this.wrongLetters.forEach(function (letter) {
+                el += `<li data-guessed-letter="${letter}" class="guessed-letter">${letter}</li>`
+            });
+
+            el += '</ul>';
+
+            this.elWrongLetters.innerHTML = el;
         },
 
         playerWins: function () {
             this.elNotices.innerHTML = "Awesome, You've Won!";
+            this.elGamesWon.innerHTML = `${numberOfWins}`;
+            console.log(numberOfWins);
+        },
+
+        resetGame: function () {
+            this.wrongLetters = [];
+            this.correctLetters = [];
+            this.remainingAttempts = 0;
+            this.elNotices.innerHTML = "";
+            this.elHint.innerHTML = "";
+            this.elWrongLetters.innerHTML = "";
+            this.init(this.words);
         },
 
     };
@@ -214,8 +279,6 @@
 
     WordGuess.init(wordList);
 
-    console.log(WordGuess);
-
-    window.addEventListener('keydown', (e) => { WordGuess.playerGuess(e) } );
+    window.addEventListener('keyup', (e) => { WordGuess.playerGuess(e) } );
 
 })(window);
